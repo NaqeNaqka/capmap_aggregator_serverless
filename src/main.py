9b91bc69eef0b14444b4ad45608073258fa8834d
NaqeNaqka <joankociko1@gmail.com>
2024-12-26 15:53:48 +0100

    reformatting

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import sys
 from aggregate import main
 from datetime import datetime
-from logging_config import setup_logging
+import time
 
-# Set up a global logger
+from logging_config import setup_logging
 logger = setup_logging()
+
+import tracemalloc
+tracemalloc.start()
+start = time.perf_counter()
 
 start_date = os.environ.get("START_DATE")
 end_date = os.environ.get("END_DATE")
@@ -36,3 +41,26 @@ logger.info(f"Starting aggregation for dates start_date: {start_date}, end_date:
 
 # Pass the parsed dates to the main function
 main(parsed_start_date, parsed_end_date)
+
+
+end = time.perf_counter()
+curr, peak = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+    
+def convert_size(size_bytes):
+    # Handle the case for 0 bytes
+    if size_bytes == 0:
+        return "0B"
+    
+    # Define the units
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB")
+    i = int((size_bytes).bit_length() - 1) // 10  # Find which unit to use
+    p = 1024 ** i
+    s = size_bytes / p
+    return f"{s:.2f} {size_name[i]}"
+
+converted_size = convert_size(peak)
+
+logger.info(f"\nFinished in {end-start:.2f}sec.")
+logger.info(f"Peak memory usage: {converted_size}.")
+sys.exit("Exiting...")
